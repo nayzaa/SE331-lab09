bf0024451120387b5071979e1096abdeee7b064d
nira <nayzaa2010@hotmail.com>
2017-11-03 14:57:37 +0700

    1.4

---
+++ web/src/app/students/course-list/course-list.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'course-list',
+  templateUrl: 'app/students/course-list/course-list.component.html',
+  styleUrls:['app/students/course-list/course-list.component.css']
+})
+export class CouresListComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit(){
+
+  }
+}
